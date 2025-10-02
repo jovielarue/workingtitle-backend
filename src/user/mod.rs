1e745da2951400043a69dff338791b125fe2ec68
Jovie LaRue <44442031+jovielarue@users.noreply.github.com>
2025-10-01 18:35:09 -0600

    Feat: get and delete user working

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -3,38 +3,102 @@ use std::sync::Arc;
 use axum::{
     Json, Router,
     extract::{Path, State},
-    http::StatusCode,
     routing::{delete, get, patch, post},
 };
+use mongodb::bson::{doc, oid::ObjectId, to_document};
 use serde::{Deserialize, Serialize};
 
-use crate::AppState;
+use crate::{
+    AppState,
+    api::types::{ApiError, ApiResponse},
+};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
-    user_id: u32,
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    user_id: Option<ObjectId>,
     first_name: String,
-    last_name: bool,
+    last_name: String,
     email: String,
 }
 
 async fn create_user(
     State(app_state): State<Arc<AppState>>,
     Json(input): Json<User>,
-) -> Result<StatusCode, StatusCode> {
+) -> Result<ApiResponse<User>, ApiError> {
     let result = app_state.users.insert_one(input).await;
 
     match result {
-        Ok(_) => Ok(StatusCode::CREATED),
-        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+        Ok(_) => Ok(ApiResponse::Created),
+        Err(_) => Err(ApiError::InternalServerError),
     }
 }
 
-async fn get_user(Path(id): Path<i32>) -> String {
-    format!("one user here with an id of {}", id)
+async fn get_user(
+    State(app_state): State<Arc<AppState>>,
+    Path(id): Path<String>,
+) -> Result<ApiResponse<User>, ApiError> {
+    match ObjectId::parse_str(id) {
+        Ok(oid) => match app_state.users.find_one(doc! {"_id": oid}).await {
+            Ok(Some(u)) => {
+                println!("User found:\n{:?}", u);
+                Ok(ApiResponse::JsonData::<User>(vec![u as User]))
+            }
+            Ok(None) => {
+                println!("User not found.");
+                Err(ApiError::NotFound)
+            }
+            Err(e) => {
+                println!("Database error: {:?}", e);
+                Err(ApiError::InternalServerError)
+            }
+        },
+        Err(e) => {
+            println!("Error parsing object id from path: {:?}", e);
+            Err(ApiError::InternalServerError)
+        }
+    }
 }
 
-async fn delete_user() {}
+async fn delete_user(
+    State(app_state): State<Arc<AppState>>,
+    Path(id): Path<String>,
+) -> Result<ApiResponse<User>, ApiError> {
+    match ObjectId::parse_str(id) {
+        Ok(oid) => {
+            match app_state.users.find_one(doc! {"_id": oid}).await {
+                Ok(Some(u)) => {
+                    println!("User found:\n{:?}\nDeleting now...", u);
+
+                    // must convert User type to mongo document for deletion
+                    let user_doc = to_document(&u).unwrap();
+                    let delete_result = app_state.users.delete_one(user_doc).await;
+
+                    match delete_result {
+                        Ok(_) => Ok(ApiResponse::Deleted),
+                        Err(e) => {
+                            println!("Error during user deletion: {:?}", e);
+                            Err(ApiError::InternalServerError)
+                        }
+                    }
+                }
+                Ok(None) => {
+                    println!("User not found.");
+                    Err(ApiError::NotFound)
+                }
+                Err(e) => {
+                    println!("Database error: {:?}", e);
+                    Err(ApiError::InternalServerError)
+                }
+            }
+        }
+        Err(e) => {
+            println!("Error parsing object id from path: {:?}", e);
+            Err(ApiError::InternalServerError)
+        }
+    }
+}
+
 async fn update_user() {}
 
 pub fn users_router(app_state: Arc<AppState>) -> Router {
